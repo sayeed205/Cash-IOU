e4841c44ae7cdd1c9fee27b1d1e680a4a0de2cb4
Hitarashi <sayeed205@gmail.com>
2023-05-11 08:04:13 +0530

    Initial Commit

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+    const [count, setCount] = useState(0);
+    const [greeting, setGreeting] = useState("");
+
+    useEffect(() => {
+        fetch("/api")
+            .then((res) => res.text())
+            .then(setGreeting);
+    }, []);
+
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+                <a href="https://nestjs.com">
+                    <img
+                        src="https://docs.nestjs.com/assets/logo-small.svg"
+                        className="logo"
+                        alt="Nest logo"
+                    />
+                </a>
+            </div>
+            <h1>Vite + React + Nest</h1>
+            <h1>{greeting}</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
+}
+
+export default App;
