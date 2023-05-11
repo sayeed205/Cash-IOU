b562ea3e1b4c8785b234e80e2f531b3e0cdd5e0e
Hitarashi <sayeed205@gmail.com>
2023-05-11 08:12:36 +0530

    build before running production add port specify

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/api');
-  await app.listen(5000);
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('/api');
+    await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
